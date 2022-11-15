b246d287e3dee2372925f68a40800c598836508d
Andreea <tatarandreea16@gmail.com>
2022-11-15 14:29:29 +0200

    Initial configuration for React and Spring

--- src/main/java/com/example/proiectlicitatii/ProiectLicitatiiApplication.java
+++ src/main/java/com/example/proiectlicitatii/ProiectLicitatiiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProiectLicitatiiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProiectLicitatiiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectLicitatiiApplication.class, args);
+    }
 
 }
