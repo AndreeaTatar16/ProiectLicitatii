dc76037e183813eacf1d2ee95a9c9e8a29f918bd
Andreea <tatarandreea16@gmail.com>
2022-11-15 14:24:52 +0200

    Initial commit

---
+++ src/test/java/com/example/proiectlicitatii/ProiectLicitatiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectlicitatii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectLicitatiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
